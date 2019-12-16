f44321d5fbd905236043d3c0a71ba758b8cc4b85
curtiscarlosja <56280651+curtiscarlosja@users.noreply.github.com>
2019-12-16 00:22:52 -0500

    Update dbconnection.php

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,5 +1,6 @@
 <?php
-$con=mysqli_connect("Localhost", "root", "", "ermsdb");
+$con=mysqli_connect("remotemysql.com", "LHxeA0Y7Ce", "HLauu4PBlM", "LHxeA0Y7Ce");
+
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
